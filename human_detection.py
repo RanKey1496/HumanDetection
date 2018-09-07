5d456fb836e381a96360cc6c0fdee62a7b0b8ffc
JHON JAIME GIL SEPULVEDA <JGS808@wnutibara.com.co>
2018-09-07 16:29:28 -0500

    First impression, it works!

---
+++ human_detection.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 13:07:43 2018
+
+@author: jgs808
+"""
+
+# In[1]:
+import cv2
+import datetime
+
+# In[2]:
+from utils import detector_utils
+detection_graph, sess = detector_utils.load_inference_graph()
+
+# In[3]:
+
+if __name__ == '__main__':
+    score_thresh = 0.70
+    
+    cap = cv2.VideoCapture(0)
+    
+    num_humans_detect = 2
+    
+    start_time = datetime.datetime.now()
+    
+    im_height, im_width = (None, None)
+    
+    try:
+        while True:
+            ret, frame = cap.read()
+            # frame = cv2.resize(frame, (320, 240))
+            
+            if im_height == None:
+                im_height, im_width = frame.shape[:2]
+                
+            try:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            except:
+                print("Error converting to RGB")
+            frame = cv2.resize(frame, (int(im_width*1.5), int(im_height*1.5)))
+            boxes, scores, classes = detector_utils.detect_objects(frame, detection_graph, sess)
+            
+            detector_utils.draw_box_on_image(num_humans_detect, score_thresh, scores, boxes, classes, im_width, im_height, frame)
+                        
+            cv2.imshow('Human detector', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+            
+    except:
+        print("Error")
---
+++ utils/detector_utils.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 13:23:47 2018
+
+@author: jgs808
+"""
+
+# In[1]:
+import numpy as np
+import sys
+import tensorflow as tf
+import os
+from threading import Thread
+from datetime import datetime
+import cv2
+from collections import defaultdict
+from object_detection.utils import label_map_util
+
+# In[2]:
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# In[3]:
+def load_inference_graph():
+    print("--- Loading frozen graph into memory ---")
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        sess = tf.Session(graph=detection_graph)
+    return detection_graph, sess
+
+# In[4]:
+def draw_box_on_image(num_humans, score_tresh, scores, boxes, classes, im_width, im_height, image_np):
+    color = (0, 50, 255)
+    for i in range(num_humans):
+        if (scores[i] > score_tresh):
+            if classes[i] == 1:
+                (left, right, top, bottom) = (boxes[i][1] * im_width, boxes[i][3] * im_width,
+                                                boxes[i][0] * im_height, boxes[i][2] * im_height)
+                p1 = (int(left), int(top))
+                p2 = (int(right), int(bottom))
+                
+                cv2.rectangle(image_np, p1, p2, color, 3, 1)
+                cv2.putText(image_np, 'Humano', (int(left), int(top)-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                cv2.putText(image_np, 'Probabilidad: ' + str("{0:.2f}".format(scores[i])), (int(left), int(top)-20),
+                                                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+# In[5]:
+def draw_text_on_image(fps, image_np):
+    cv2.putText(image_np, fps, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (77, 255, 9), 2)
+    
+# In[6]:
+def detect_objects(image_np, detection_graph, sess):
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    
+    (boxes, scores, classes, num) = sess.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+    return np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes)
